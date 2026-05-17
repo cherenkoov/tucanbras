bfbe8c58cae649c5c9f68f8cd1c7b0f2653dc85f
cherenkov <musical.1998.y@gmail.com>
2026-05-17 20:13:47 +0400

    feat: install gsap, add SVG preload hint in layout

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -13,6 +13,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
+      <head>
+        <link
+          rel="preload"
+          href="/SVG/background/background [Vectorized].svg"
+          as="fetch"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}</body>
     </html>
   );
